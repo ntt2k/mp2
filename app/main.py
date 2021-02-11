cfd7512ce2e8d49c06873925b450afb5770ed2d7
Trung Nguyen <trung.nt2k@gmail.com>
2021-02-10 20:45:31 -0600

    update

--- app/main.py
+++ app/main.py
@@ -14,11 +14,11 @@ class Number(BaseModel):
 def read_num():
     with open("num.txt", "r") as f:
         num = f.read()
-    return num
+    return {"num": num}
 
 
 @app.post("/")
 def save_num(n: Number):
     with open("num.txt", "w") as f:
         f.write(str(n.num))
-    return n.num
+    return str(n.num)

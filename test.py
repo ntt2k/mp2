271496afbb07ff932a0bf3fd4478f0282462914f
Trung Nguyen <trung.nt2k@gmail.com>
2021-02-10 21:55:09 -0600

    update

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ import json
 url = 'https://seorwrpmwh.execute-api.us-east-1.amazonaws.com/prod/mp1'
 
 payload = {
-		'ip_address1':  '34.207.67.255:80',
-		'ip_address2':  '3.93.172.163:80',
-		'load_balancer' :  'ec2-docker-fastapi-933855433.us-east-1.elb.amazonaws.com',
-		'submitterEmail':  'trungtn2@illinois.edu',
-		'secret':  'gTlsKhI3ELSKbdjB'
+		'ip_address1':'34.207.67.255:80',
+		'ip_address2':'3.93.172.163:80',
+		'load_balancer':'lb-ec2-docker-fastapi-2099620855.us-east-1.elb.amazonaws.com:80',
+		'submitterEmail':'trungtn2@illinois.edu',
+		'secret':'WSmZyPovQV6ZkLSh'
 		}
 
 r = requests.post(url, data=json.dumps(payload))
